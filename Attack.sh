472efba9465578277b3472b67b68efd576bf20c3
Tnbcd <89185811+Tnbcd@users.noreply.github.com>
2021-08-19 19:20:02 +0700

    Add files via upload

---
+++ Attack.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+echo -e "#############################"
+echo -e " AUTHOR : MR BCD "
+echo -e "#############################"
+read -p "masukan input di sini:" no
+while [[ true ]]; do
+echo "send to virus Attack $no SUCCESS"
+done 
